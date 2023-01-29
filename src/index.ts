6ce82e4c70465020809041b13e2fa7e253cadd7c
nc1z <ncxwork@gmail.com>
2023-01-29 15:19:17 +0800

    refactor imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv");
-const { fetchResponse } = require("./functions/gpt3-text-babbage-001-api.ts");
+const { fetchResponse } = require("./functions/gpt3-text-babbage-001-api");
 dotenv.config();
 
 // TELEGRAM BOT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
